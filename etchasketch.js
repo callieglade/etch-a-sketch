dc50c0c288c872d6a3dbdf043785ae09b8a224f4
Callie Glade <callieglade@gmail.com>
2020-01-15 21:15:49 -0800

    Fix .border to specify style.border instead

--- etchasketch.js
+++ etchasketch.js
@@ -7,6 +7,8 @@ for (let col = 0; col < 16; col++) {
     for (let row = 0; row < 16; row++) {
         gridItems[col[row]] = document.createElement("div");
         gridItems[col[row]].classList.add("grid-item");
+        gridItems[col[row]].style.border = '1px solid black';
+        gridItems[col[row]].textContent = col + "," + row;
         container.appendChild(gridItems[col[row]]);
     }
 }
