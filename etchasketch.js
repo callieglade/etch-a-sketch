9ca5e018e075460ee36cdfec310bc1f8af7bf35a
Callie Glade <callieglade@gmail.com>
2020-01-23 00:33:48 -0800

    Add .hover to CSS and set mouseenter and mouseleave events to add/remove class

--- etchasketch.js
+++ etchasketch.js
@@ -8,7 +8,14 @@ for (let col = 0; col < 16; col++) {
         gridItems[col[row]] = document.createElement("div");
         gridItems[col[row]].classList.add("grid-item");
         gridItems[col[row]].style.border = '1px solid black';
-        gridItems[col[row]].textContent = col + "," + row;
+        gridItems[col[row]].addEventListener('mouseenter', function(e) {
+            e.target.classList.add("hover");
+        });
+        gridItems[col[row]].addEventListener('mouseleave', function(e) {
+            setTimeout(() => {
+                e.target.classList.remove("hover");
+            }, 500);
+        });
         container.appendChild(gridItems[col[row]]);
     }
 }
